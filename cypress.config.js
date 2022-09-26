2bd7835b827d6d896847fbd09d2f68154febc60a
GomenaSorry <ruzzellfernandez28@gmail.com>
2022-09-26 23:44:18 +0800

    commented Cucumber configs due to conflict with neat-csv, added neat-csv package

--- cypress.config.js
+++ cypress.config.js
@@ -2,42 +2,44 @@ const { defineConfig } = require("cypress");
 const webpack = require("@cypress/webpack-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
-async function setupNodeEvents(on, config) {
-  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+// uncomment for setupNodeEvents for BDD tests
+// async function setupNodeEvents(on, config) {
+//   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+//   await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
-  on(
-    "file:preprocessor",
-    webpack({
-      webpackOptions: {
-        resolve: {
-          extensions: [".ts", ".js"],
-        },
-        module: {
-          rules: [
-            {
-              test: /\.feature$/,
-              use: [
-                {
-                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
-                  options: config,
-                },
-              ],
-            },
-          ],
-        },
-      },
-    })
-  );
+//   on(
+//     "file:preprocessor",
+//     webpack({
+//       webpackOptions: {
+//         resolve: {
+//           extensions: [".ts", ".js"],
+//         },
+//         module: {
+//           rules: [
+//             {
+//               test: /\.feature$/,
+//               use: [
+//                 {
+//                   loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+//                   options: config,
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       },
+//     })
+//   );
 
-  // Make sure to return the config object as it might have been modified by the plugin.
-  return config;
-}
+//   // Make sure to return the config object as it might have been modified by the plugin.
+//   return config;
+// }
 
 module.exports = defineConfig({
   projectId: 'dqy8am',
   e2e: {
-    setupNodeEvents,
+    // uncomment for setupNodeEvents for BDD tests
+    // setupNodeEvents,
     // non-BDD tests
     specPattern: 'cypress/integration/examples/*.js'
     // BDD tests
@@ -48,5 +50,6 @@ module.exports = defineConfig({
   },
   // reporter: 'mochawesome',
   defaultCommandTimeout: 10000,
-  pageLoadTimeout: 30000
+  pageLoadTimeout: 30000,
+  downloadsFolder: 'cypress/downloads'
 });
